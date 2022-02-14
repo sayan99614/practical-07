106a48e41da6f4e50a68bd95c473523cd390c327
dheeman pati <dheemanpati53@gmail.com>
2022-02-14 16:37:36 +0530

    added user hover card

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./maincard.css";
 import UserProfile from "./UserProfile";
+import { data } from "./data";
+import Avatar from "react-avatar";
 function MainCard(props) {
+  const [hover, setHover] = useState(false);
+  const [showId, setShowId] = useState(null);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const singledata = data.find((item) => showId === item.id);
+    setUser(singledata);
+    console.log(user);
+  });
+
   return (
     <div className="row">
       <div className="col-sm-8 offset-sm-2">
         <div className="card card-body shadow-sm p-5 mt-5">
-          <table class="table table-borderless padding-table-columns">
+          <table className="table table-borderless padding-table-columns">
             <thead>
               <tr>
                 <th scope="col">Name</th>
@@ -16,16 +28,31 @@ function MainCard(props) {
               </tr>
             </thead>
             <tbody>
-              <UserProfile isOwner={true} />
-              <UserProfile />
-              <UserProfile />
-              <UserProfile />
-              <UserProfile />
-              <UserProfile />
-              <UserProfile />
-              <UserProfile />
+              {data.map((item) => {
+                return (
+                  <UserProfile
+                    isOwner={item.id === 1 ? true : false}
+                    key={item.id}
+                    first_name={item.first_name}
+                    email={item.email}
+                    last_name={item.last_name}
+                    avatar={item.avatar}
+                    id={item.id}
+                    hover={hover}
+                    setHover={setHover}
+                    setShowId={setShowId}
+                  />
+                );
+              })}
             </tbody>
           </table>
+
+          {hover && (
+            <div className="card shadow-lg p-3 hovercard text-center">
+              <Avatar round size="100" src={user.avatar} />
+              <h6 className="mt-2">{`${user.first_name} ${user.last_name}`}</h6>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "react-avatar";
 import "./userprofile.css";
-function UserProfile({ isOwner }) {
+function UserProfile({
+  isOwner,
+  email,
+  first_name,
+  last_name,
+  avatar,
+  id,
+  hover,
+  setHover,
+  setShowId,
+}) {
+  function mouseEnter(id) {
+    setShowId(id);
+    setHover(true);
+  }
+
+  function mouseOut() {
+    setHover(false);
+  }
+
   return (
     <tr>
       <th>
         <div className="d-flex">
-          <div className="avatar">
-            <Avatar
-              round
-              size="35"
-              src="https://reqres.in/img/faces/2-image.jpg"
-            />
+          <div
+            className="avatar"
+            onMouseEnter={() => mouseEnter(id)}
+            onMouseLeave={mouseOut}
+          >
+            <Avatar round size="35" src={avatar} />
           </div>
           <div className="d-flex flex-column">
-            <div className="name">George Bluth</div>
-            <div className="email text-secondary">george.bluth@reqres.in</div>
+            <div className="name">{`${first_name} ${last_name}`}</div>
+            <div className="email text-secondary">{email}</div>
           </div>
         </div>
       </th>
       <td>
-        <select className="form-select w-75 form-control-sm" id="example1">
+        <select className="form-select form-control-sm" id="example1">
           <option>1</option>
           <option>2</option>
         </select>
       </td>
       <td>
-        <select className="form-select w-75 form-control-sm">
+        <select className="form-select form-control-sm">
           <option>1</option>
           <option>2</option>
         </select>
       </td>
       <td>
         {isOwner ? (
-          <i class="fa-solid fa-lock fa-lg text-secondary" />
+          <i className="fa-solid fa-lock fa-lg text-secondary" />
         ) : (
-          <i class="fa-solid fa-trash-can fa-lg text-secondary"></i>
+          <i className="fa-solid fa-trash-can fa-lg text-secondary"></i>
         )}
       </td>
     </tr>
