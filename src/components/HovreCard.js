1c37daa2a323dc9ac4909e2cf80926091935fd3f
dheeman pati <dheemanpati53@gmail.com>
2022-02-24 15:35:37 +0530

    added dynamic data in hover card

---
+++ src/components/HovreCard.js
@@ -0,0 +1,49 @@
+import React, { useMemo } from "react";
+import Avatar from "react-avatar";
+import { Line } from "rc-progress";
+function HovreCard({ user }) {
+  let usage = [35, 45, 65, 55, 75, 89];
+  let clicks = [2500, 3000, 3500, 4000, 5000];
+  let monthlyClicks = [5000, 6000, 7000, 8000, 9000, 10000];
+  const data = useMemo(() => {
+    return {
+      usagep: usage[Math.floor(Math.random() * usage.length)],
+      mClick: monthlyClicks[Math.floor(Math.random() * monthlyClicks.length)],
+      clk: clicks[Math.floor(Math.random() * clicks.length)],
+    };
+  }, []);
+  return (
+    <div className="card shadow-lg p-3 hovercard usercard">
+      <div className="text-center">
+        <Avatar round size="100" src={user.avatar} />
+        <h6 className="mt-2">{`${user.first_name} ${user.last_name}`}</h6>
+        <p className="userp">{user.email}</p>
+        <p className="mt-1">Your Plan: Standard</p>
+        <button className="btn btn-warning w-75 text-white">Active User</button>
+        <div className="mt-3">
+          <p>Plan Uses</p>
+          <Line
+            percent={data.usagep}
+            strokeWidth="2"
+            strokeColor="#feb23f"
+            className="mt-3 w-75 mx-2"
+          />
+        </div>
+
+        <div className="d-flex justify-content-center mt-3">
+          <div className="d-flex flex-column line-height">
+            <p className="times">{data.clk}</p>
+            <p className="desc">clicks reviewed</p>
+          </div>
+          <div className="d-flex flex-column devider"></div>
+          <div className="d-flex flex-column line-height">
+            <p className="times">{data.mClick}</p>
+            <p className="desc">monthly clicks</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HovreCard;
--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,12 +1,11 @@
 import "./maincard.css";
 import UserProfile from "./UserProfile";
-import Avatar from "react-avatar";
-import { Line } from "rc-progress";
 import { connect, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchUsers, setList } from "../Actions/actions";
 import { SpinnerCircularFixed } from "spinners-react";
 import Pagination from "./Pagination";
+import HovreCard from "./HovreCard";
 function MainCard(props) {
   const { card, user, data, loading, error } = props;
   const dispatch = useDispatch();
@@ -56,40 +55,7 @@ function MainCard(props) {
             </table>
           )}
 
-          {card && user && (
-            <div className="card shadow-lg p-3 hovercard usercard">
-              <div className="text-center">
-                <Avatar round size="100" src={user.avatar} />
-                <h6 className="mt-2">{`${user.first_name} ${user.last_name}`}</h6>
-                <p className="userp">{user.email}</p>
-                <p className="mt-1">Your Plan: Standard</p>
-                <button className="btn btn-warning w-75 text-white">
-                  Active User
-                </button>
-                <div className="mt-3">
-                  <p>Plan Uses</p>
-                  <Line
-                    percent={25}
-                    strokeWidth="2"
-                    strokeColor="#feb23f"
-                    className="mt-3 w-75 mx-2"
-                  />
-                </div>
-
-                <div className="d-flex justify-content-center mt-3">
-                  <div className="d-flex flex-column line-height">
-                    <p className="times">2500</p>
-                    <p className="desc">clicks reviewed</p>
-                  </div>
-                  <div className="d-flex flex-column devider"></div>
-                  <div className="d-flex flex-column line-height">
-                    <p className="times">5000</p>
-                    <p className="desc">monthly clicks</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
+          {card && user && <HovreCard user={user} />}
           <Pagination page={props.page} total_pages={props.total_pages} />
         </div>
       </div>
